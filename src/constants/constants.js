a4ac9767cc94420383ec4f8f6660740d78502ccf
Sasha Vishnevetskaya <visneveckaya.a@gmail.com>
2022-08-24 21:58:04 +0300

    front-version-1

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 export const URL_FILM = "https://api.nomoreparties.co/beatfilm-movies";
 export const MOVIES_SERVER_URL = "https://api.nomoreparties.co";
 export const BASE_URL = process.env.NODE_ENV === "production"
-? "https://api.sashavishnea.movies.nomoredomains.xyz;"
+? "https://api.sashavishnea.movies.nomoredomains.xyz"
 : "http://localhost:3001";
 export const NOT_FOUND = "Ничего не найдено";
 export const SERVER_ERR = "Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз";
